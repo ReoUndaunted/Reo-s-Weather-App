9d73024cc546b0527d1f3557f52595235fdef081
Raphael Ovonlen <reoundaunted@gmail.com>
2022-11-18 00:46:46 +0100

    Completed weather app

--- js/app.js
+++ js/app.js
@@ -15,13 +15,14 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?l" + name + "')"
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
@@ -37,4 +38,6 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
   if (event.key == "Enter") {
     weather.search();
   }
-})
+});
+
+weather.fetchWeather("Lagos");
